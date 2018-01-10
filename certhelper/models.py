372704d32732e8a242c81d20c34b8b95545bca2f
Filip Ilic <filip.ilic@cern.ch>
2018-01-10 15:52:18 +0100

    documentation

--- certhelper/models.py
+++ certhelper/models.py
@@ -1,3 +1,19 @@
+"""
+Models define the database schema and the data that is stored in the database.
+For (most) of the models there is a corresponding FORM so that it is possible
+for users to add data to the model and to display it
+TkDQMDoctor/dqmsite/certhelper/forms.py.
+i.e
+
+FORM               |  MODEL
+====================================
+ReferenceRunForm   |  ReferenceRun
+RunInfoForm        |  RunInfo
+TypeForm           |  Type
+
+
+"""
+
 from django.db import models
 
 RECO_CHOICES       = (('Express','Express'),('Prompt','Prompt'), ('reReco', 'reReco'))
@@ -20,7 +36,7 @@ class Type(models.Model):
     def __str__(self):
         return str(self.reco) + " " + str(self.runtype) + " " + str(self.bfield) + " " + str(self.beamtype) + " " + str(self.beamenergy) + " "  + str(self.dataset)
 
-
+# ReferenceRun that should only be added by shift-leaders / staff
 class ReferenceRun(models.Model):
     reference_run  = models.IntegerField()
     reco                  = models.CharField(max_length=30, choices=RECO_CHOICES)
@@ -36,6 +52,7 @@ class ReferenceRun(models.Model):
     def __str__(self):
         return str(self.reference_run) + " " +  str(self.reco) + " " + str(self.runtype) + " " + str(self.bfield) + " " + str(self.beamtype) + " " + str(self.beamenergy) + " " + str(self.dataset)
 
+# Runs that shifters are certifying
 class RunInfo(models.Model):
     GOOD_BAD_CHOICES  = (('Good','Good'), ('Bad','Bad'), ('Lowstat','Lowstat'), ('Excluded','Excluded'))
     TRACKERMAP_CHOICES  = (('Exists','Exists'),('Missing','Missing'))
@@ -56,7 +73,6 @@ class RunInfo(models.Model):
 
     def __str__(self):
         return str(self.run_number)
-
 
 
 
--- certhelper/views.py
+++ certhelper/views.py
@@ -14,27 +14,43 @@ from .forms import *
 from .tables import *
 
 class CreateRun(generic.CreateView):
+    """Form which allows for creation of a new entry in RunInfo
+    """
+
     model = RunInfo
     form_class = RunInfoForm
     template_name_suffix = '_form'
     success_url = '/'
 
 def listruns(request):
+    """passes all RunInfo objects to list.html
+    """
+
     table = RunInfoTable(RunInfo.objects.all())
     RequestConfig(request).configure(table)
     return render(request, 'certhelper/list.html', {'table': table})
 
 class ListReferences(SingleTableView):
+    """Display ReferenceRuns in a tableview
+    !!! USES DJANGO-TABLES2 !!! 
+    """
+
     model = ReferenceRun
     table_class = ReferenceRunTable
 
 class UpdateRun(generic.UpdateView):
+    """Updates a specific Run from the RunInfo table
+    """
+
     model = RunInfo
     form_class = RunInfoForm
     success_url = '/'
     template_name = 'certhelper/runinfo_form.html'
 
 class DeleteRun(generic.DeleteView):
+    """Deletes a specific Run from the RunInfo table    
+    """
+
     model = RunInfo
     form_class = RunInfoForm
     success_url = '/'
@@ -42,12 +58,20 @@ class DeleteRun(generic.DeleteView):
 
 
 class CreateType(generic.CreateView):
+    """Form to create a new Type (RunType)
+    """
+
     model = Type
     form_class = TypeForm
     template_name_suffix = '_form'
     success_url = '/create'
 
 class SummaryView(generic.ListView):
+    """ Accumulates information that is needed in the Run Summary
+    stores it in the 'context' object and passes that object to summary.html
+    where it is then displayed.
+    """
+
     template_name = 'certhelper/summary.html'
     context_object_name = 'runs'
 
@@ -66,17 +90,10 @@ class SummaryView(generic.ListView):
         good_runs = tmpsorted.filter(pixel="Good",sistrip="Good",tracking="Good")
         bad_runs = tmpsorted.filter(Q(pixel="Bad") | Q(sistrip="Bad") | Q(tracking="Bad"))
 
-        
-        # context['sums'] = RunInfo.objects.raw("""SELECT *,((pixel='Good' or pixel='Lowstat') and (sistrip='Good' or sistrip='Lowstat') and (tracking='Good' or tracking='Lowstat')) as good, 
-        #                                                    SUM(number_of_ls) as sum_number_of_ls,
-        #                                                    SUM(int_luminosity) as sum_int_luminosity 
-        #                                                    FROM certhelper_runinfo a 
-        #                                                    inner join certhelper_type b 
-        #                                                    on a.type_id = b.ID 
-        #                                                    group by b.ID, good 
-        #                                                    order by type_id, -good""")
-
-
+        # General Remark:
+        # These are SQL queries that you can copy paste into your dbtool.
+        # Using the inbuilt Django functions is safer but does not give you as much control
+ 
         context['sums'] = RunInfo.objects.raw("""SELECT *,
         case runtype
             when 'Collisions' then  ((pixel='Good' or pixel='Lowstat') and (sistrip='Good' or sistrip='Lowstat') and (tracking='Good' or tracking='Lowstat'))
@@ -110,6 +127,9 @@ class SummaryView(generic.ListView):
 
 
 def clearsession(request):
+    """Deletes all the entries in the RunInfo table and redicrets to '/'.
+    """
+
     if request.method == 'POST':
         RunInfo.objects.all().delete()
         return HttpResponseRedirect('/')
@@ -119,5 +139,8 @@ def clearsession(request):
 
 
 def logout_view(request):
+    """ Logout current user
+    """
+        
     logout(request)
     return HttpResponseRedirect('/')
