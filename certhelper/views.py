b5aa65064cc02ad98e76b70c7407e2fcc0b379af
Peter Stein <peter.stein@cern.ch>
2018-03-27 16:48:48 +0200

    Replace JavaScript AsciiTable with Python terminaltables
    JavaScript is throwing a "Uncaught syntax error: invalid or unexpected
    token" when a newline character is inside the comments box.
    
    Since the Javascript AsciiTable library is incapable of handling Multiline Strings,
    it is replaced by the Python package "terminaltables"

--- certhelper/views.py
+++ certhelper/views.py
@@ -14,6 +14,8 @@ from .models import *
 from .forms import *
 from .tables import *
 
+from terminaltables import AsciiTable
+
 class CreateRun(generic.CreateView):
     """Form which allows for creation of a new entry in RunInfo
     """
@@ -84,24 +86,51 @@ class CreateType(generic.CreateView):
     template_name_suffix = '_form'
     success_url = '/create'
 
+class SummaryAsciiTable:
+    """
+    Used to print the AsciiTables in the summary view
+    """
+
+    def __init__(self, thetype, tableColumnDescriptions):
+        self.mytype = thetype.__str__()
+        self.data = [tableColumnDescriptions]
+
+    def add_row(self, row):
+        self.data.append(row)
+
+    def print(self):
+        print(self.mytype)
+        table = AsciiTable(self.data)
+        table.inner_row_border = True
+        print(table.table)
+
+    def get_type(self):
+        return self.mytype
+
+    def get_table(self):
+        table = AsciiTable(self.data)
+        table.inner_row_border = True
+        return table.table
+
 class SummaryView(generic.ListView):
     """ Accumulates information that is needed in the Run Summary
     stores it in the 'context' object and passes that object to summary.html
     where it is then displayed.
     """
-
     template_name = 'certhelper/summary.html'
     context_object_name = 'runs'
 
     def get_queryset(self):
         return RunInfo.objects.filter(userid=self.request.user)
-    
+
     def get_context_data(self, **kwargs):
         context = super(SummaryView, self).get_context_data(**kwargs)
 
+        """Retrieve all runs from the current user"""
         CurrentUserRunInfos = RunInfo.objects.filter(userid=self.request.user)
 
         ids = CurrentUserRunInfos.values_list('reference_run').distinct()
+
         context['refs'] = ReferenceRun.objects.filter(id__in=ids)
 
         context['sorted_by_type'] = CurrentUserRunInfos.order_by('type')
@@ -110,6 +139,46 @@ class SummaryView(generic.ListView):
         good_runs = tmpsorted.filter(pixel="Good",sistrip="Good",tracking="Good")
         bad_runs = tmpsorted.filter(Q(pixel="Bad") | Q(sistrip="Bad") | Q(tracking="Bad"))
 
+        userRunInfoTypes = RunInfo \
+            .objects \
+            .filter(userid=self.request.user) \
+            .values('type') \
+            .order_by('type', 'run_number') \
+            .distinct()
+
+        columnDescription = ["Run", "Reference Run", "Number of LS", "Int. Luminosity", "Pixel", "Strip", "Tracking",
+                             "Comment"]
+
+        """ Get a list of unique type ids (for cosmics, prompt, etc.)"""
+        type_id_list = []
+        for entry in userRunInfoTypes:
+            if entry['type'] not in type_id_list:
+                type_id_list.append(entry['type'])
+
+        """ Create one AsciiTable per Type and fill it with the appropriate data"""
+        ascii_tables = []
+        for type_id in type_id_list:
+            userRunInfosPerType = CurrentUserRunInfos.filter(type_id=type_id).order_by("run_number")
+
+            """Extract the type from the first element, all types in that list are the same"""
+            the_type = userRunInfosPerType[0].type
+            """Create a AsciiTable with just the column descriptions as the first row"""
+            ascii_tables.append(SummaryAsciiTable(the_type, columnDescription))
+            for run in userRunInfosPerType:
+                """ add a new row to the lastly created AsciiTable"""
+                ascii_tables[-1].add_row([
+                    run.run_number,
+                    run.reference_run.reference_run,
+                    run.number_of_ls,
+                    run.int_luminosity,
+                    run.pixel,
+                    run.sistrip,
+                    run.tracking,
+                    run.comment
+                ])
+
+        """Provide the context for the summary template"""
+        context["ascii_tables"] = ascii_tables
 
         # ====================================================================================================================
          
