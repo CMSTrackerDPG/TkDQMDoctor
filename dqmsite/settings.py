4953b8745965b1547a3c97244a60e1bb17391cb1
Peter Stein <peter.stein@cern.ch>
2018-04-25 11:31:42 +0200

    change database from sqlite3 to PostgreSQL

--- dqmsite/settings.py
+++ dqmsite/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'dqmsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DJANGO_DATABASE_NAME'),
+        'USER': os.environ.get('DJANGO_DATABASE_USER'),
+        'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DJANGO_DATABASE_HOST'),
+        'PORT': os.environ.get('DJANGO_DATABASE_PORT'),
+    },
 }
 
 # Password validation
