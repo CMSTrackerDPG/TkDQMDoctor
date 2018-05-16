e83c7947a1f55de489b35bd4914a6de0b4a955ef
Peter Stein <peter.stein@cern.ch>
2018-05-16 15:14:47 +0200

    fix bug that causes deletion of every run in the table
    the colum should not have the name "delete" when it is overwritten by a
    Model.
    
    See https://github.com/jieter/django-tables2/issues/353 for more details

--- certhelper/tables.py
+++ certhelper/tables.py
@@ -3,23 +3,23 @@ from django_tables2.utils import A
 from .models import RunInfo, ReferenceRun
 
 class RunInfoTable(tables.Table):
-    edit   = tables.TemplateColumn('<div align="center"><a href="{% url \'certhelper:update\' record.id%}"><span class="glyphicon glyphicon-pencil"></a></div>', orderable=False)
-    delete = tables.TemplateColumn('<div align="center"><a href="{% url \'certhelper:delete\' record.id%}"><span class="glyphicon glyphicon-trash"></a></div>', orderable=False)
+    edit_run = tables.TemplateColumn('<div align="center"><a href="{% url \'certhelper:update\' record.id%}"><span class="glyphicon glyphicon-pencil"></a></div>', orderable=False)
+    delete_run = tables.TemplateColumn('<div align="center"><a href="{% url \'certhelper:delete\' record.id%}"><span class="glyphicon glyphicon-trash"></a></div>', orderable=False)
 
     class Meta:
         model = RunInfo
         fields = ('userid',
-        'run_number', 
-        'type',
-        'reference_run.reference_run',
-        'trackermap',
-        'number_of_ls',
-        'int_luminosity',
-        'pixel',
-        'sistrip',
-        'tracking',
-        'comment',
-        'date')
+                  'run_number',
+                  'type',
+                  'reference_run.reference_run',
+                  'trackermap',
+                  'number_of_ls',
+                  'int_luminosity',
+                  'pixel',
+                  'sistrip',
+                  'tracking',
+                  'comment',
+                  'date')
         attrs = {'class': 'table table table-hover table-bordered'}
 
 
@@ -28,17 +28,17 @@ class READONLYRunInfoTable(tables.Table):
     class Meta:
         model = RunInfo
         fields = ('userid',
-        'run_number', 
-        'type',
-        'reference_run.reference_run',
-        'trackermap',
-        'number_of_ls',
-        'int_luminosity',
-        'pixel',
-        'sistrip',
-        'tracking',
-        'comment',
-        'date')
+                  'run_number',
+                  'type',
+                  'reference_run.reference_run',
+                  'trackermap',
+                  'number_of_ls',
+                  'int_luminosity',
+                  'pixel',
+                  'sistrip',
+                  'tracking',
+                  'comment',
+                  'date')
         attrs = {'class': 'table table table-hover table-bordered'}
 
 
