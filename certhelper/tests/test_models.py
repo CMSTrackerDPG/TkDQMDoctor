0ccdd1086ea2a370a4d6e7fda5fc29dfcdb6aac5
Peter Stein <peter.stein@cern.ch>
2018-06-11 19:08:28 +0200

    Test models

--- certhelper/tests/test_models.py
+++ certhelper/tests/test_models.py
@@ -1,7 +1,12 @@
+from typing import Optional, Any
+
 import pytest
+from django.core.exceptions import ValidationError
 from mixer.backend.django import mixer
 
 from certhelper.models import RunInfo
+from certhelper.models import ReferenceRun
+from certhelper.models import Type
 
 pytestmark = pytest.mark.django_db
 
@@ -63,3 +68,67 @@ class TestRuninfo:
         assert run.is_bad() is False
 
         assert len(RunInfo.objects.all()) == 7
+
+    def test_delete(self):
+        run = mixer.blend('certhelper.RunInfo', run_number=123456)
+
+        assert RunInfo.objects.filter(run_number=123456).exists() is True
+        assert run.run_number == 123456
+        run.delete()
+        assert RunInfo.objects.filter(run_number=123456).exists() is False
+        assert RunInfo.all_objects.filter(run_number=123456).exists() is True
+        assert RunInfo.all_objects.get(run_number=123456).pk == run.pk
+        run.restore()
+        assert RunInfo.objects.filter(run_number=123456).exists() is True
+        assert RunInfo.all_objects.filter(run_number=123456).exists() is True
+        run.hard_delete()
+        assert RunInfo.objects.filter(run_number=123456).exists() is False
+        assert RunInfo.all_objects.filter(run_number=123456).exists() is False
+        assert run.run_number == 123456
+        run.save()
+        assert RunInfo.objects.filter(run_number=123456).exists() is True
+        assert RunInfo.all_objects.filter(run_number=123456).exists() is True
+
+    def test_to_string(self):
+        run = mixer.blend('certhelper.RunInfo',
+                          run_number=123456,
+                          type__runtype="Cosmics",
+                          type__reco="Express",
+                          reference_run__reference_run=654321,
+                          reference_run__reco="Prompt",
+                          reference_run__runtype="Collisions",
+                          )
+        assert run.run_number == 123456
+        assert run.type.runtype == "Cosmics"
+        assert run.type.reco == "Express"
+        assert run.reference_run.reference_run == 654321
+        assert run.reference_run.reco == "Prompt"
+        assert run.reference_run.runtype == "Collisions"
+
+        assert str(run) == "123456, Cosmics Express (ref: 654321, Collisions Prompt)"
+        assert "654321 Prompt Collisions" in str(run.reference_run)
+        assert "Express Cosmics" in str(run.type)
+
+    def test_validate_unique(self):
+        with pytest.raises(ValidationError):
+            t: Type = mixer.blend('certhelper.Type')
+            ref: ReferenceRun = mixer.blend('certhelper.ReferenceRun')
+
+            r1 = mixer.blend('certhelper.RunInfo',
+                             run_number=123456,
+                             type=t,
+                             reference_run=ref
+                             )
+
+            r2 = mixer.blend('certhelper.RunInfo',
+                             run_number=123456,
+                             type=t,
+                             reference_run=ref
+                             )
+
+
+class TestCategory:
+    def test_category_str(self):
+        assert str(mixer.blend("certhelper.Category", name="cat1")) == "cat1"
+        assert str(mixer.blend("certhelper.SubCategory", name="subcat1")) == "subcat1"
+        assert str(mixer.blend("certhelper.SubSubCategory", name="subsubcat1")) == "subsubcat1"
