52b6764df68d95a9833cdf454fb5b0b8aa5dde53
Peter Stein <peter.stein@cern.ch>
2018-06-25 16:39:45 +0200

    Make UserProfile readonly

--- certhelper/admin.py
+++ certhelper/admin.py
@@ -4,12 +4,17 @@ from .models import *
 
 
 class UserProfileInline(admin.StackedInline):
+    """
+    Should never be filled out manually, will be automatically created and
+    filled by CERN e-groups the user is member off
+    """
     model = UserProfile
+    readonly_fields = ('extra_data', 'user_privilege',)
     can_delete = False
 
 
 class UserAdmin(BaseUserAdmin):
-    inlines = (UserProfileInline, )
+    inlines = (UserProfileInline,)
 
 
 class ReferenceRunAdmin(admin.ModelAdmin):
