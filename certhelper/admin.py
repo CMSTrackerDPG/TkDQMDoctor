d540f3ba58094fe14742bad4194a2e11a9f234fb
Peter Stein <peter.stein@cern.ch>
2018-07-05 18:32:35 +0200

    Remove old Category models from admin site

--- certhelper/admin.py
+++ certhelper/admin.py
@@ -29,24 +29,9 @@ class TypeAdmin(admin.ModelAdmin):
     exclude = ['created_at', 'updated_at', 'deleted_at']
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    exclude = ['created_at', 'updated_at', 'deleted_at']
-
-
-class SubCategoryAdmin(admin.ModelAdmin):
-    exclude = ['created_at', 'updated_at', 'deleted_at']
-
-
-class SubSubCategoryAdmin(admin.ModelAdmin):
-    exclude = ['created_at', 'updated_at', 'deleted_at']
-
-
 # Register your models here.
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(ReferenceRun, ReferenceRunAdmin)
 admin.site.register(RunInfo, RunInfoAdmin)
 admin.site.register(Type, TypeAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(SubCategory, SubCategoryAdmin)
-admin.site.register(SubSubCategory, SubSubCategoryAdmin)
